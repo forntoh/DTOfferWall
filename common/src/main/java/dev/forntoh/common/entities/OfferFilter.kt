8f52a5abe3dd21c9b2d0b302da604b5193112af6
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-15 22:22:55 +0100

    added clearPrev in filter

--- common/src/main/java/dev/forntoh/common/entities/OfferFilter.kt
+++ common/src/main/java/dev/forntoh/common/entities/OfferFilter.kt
@@ -1,8 +1,9 @@
 package dev.forntoh.common.entities
 
 data class OfferFilter(
-    var appId: String,
-    var userId: String,
-    var token: String,
+    var appId: String = "",
+    var userId: String = "",
+    var token: String = "",
     var page: Int = 1,
+    val clearPrev: Boolean = false,
 )
