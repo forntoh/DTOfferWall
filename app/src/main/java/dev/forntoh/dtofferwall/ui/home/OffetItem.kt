095efaa643fb337771c5ef4fae841c31709ce74c
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-15 21:26:20 +0100

    offeritem design

---
+++ app/src/main/java/dev/forntoh/dtofferwall/ui/home/OffetItem.kt
@@ -0,0 +1,76 @@
+package dev.forntoh.dtofferwall.ui.home
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import dev.forntoh.common.entities.Offer
+import dev.forntoh.dtofferwall.utils.OnBottomReached
+
+@Composable
+fun OfferList(
+    offerList: List<Offer>,
+    loadMore: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    val listState = rememberLazyListState()
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(16.dp),
+        state = listState,
+        modifier = modifier
+    ) {
+        items(offerList) { offer ->
+            OfferItem(offer = offer)
+        }
+    }
+
+    listState.OnBottomReached(buffer = 2, loadMore)
+}
+
+@Composable
+fun OfferItem(
+    offer: Offer,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.height(84.dp)
+        ) {
+            AsyncImage(
+                model = offer.thumbnail.hires,
+                contentDescription = offer.title,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = offer.title,
+                    style = MaterialTheme.typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+    }
+}
