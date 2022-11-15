e9ca13199a31105fc2cc1e4c38626f7f76a28785
forntoh <thomasforntoh@gmail.com>
2022-11-15 16:06:33 +0100

    added base scaffold

--- app/src/main/java/dev/forntoh/dtofferwall/ui/MainActivity.kt
+++ app/src/main/java/dev/forntoh/dtofferwall/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import dev.forntoh.dtofferwall.ui.home.HomeScreen
 import dev.forntoh.dtofferwall.ui.theme.DTOfferWallTheme
@@ -17,7 +20,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DTOfferWallTheme {
-                HomeScreen(offerWallViewModel = offerWallViewModel)
+                Scaffold { innerPaddingModifier ->
+                    HomeScreen(
+                        offerWallViewModel = offerWallViewModel,
+                        modifier = Modifier.padding(innerPaddingModifier)
+                    )
+                }
             }
         }
     }
