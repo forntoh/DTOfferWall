7e6a98ca7ad9cc76803e8dcee35179230bafee4b
forntoh <thomasforntoh@gmail.com>
2022-11-15 14:33:51 +0100

    updated sha1 generator

---
+++ common/src/main/java/dev/forntoh/common/lib/OfferWallHashKeyGenerator.kt
@@ -0,0 +1,32 @@
+package dev.forntoh.common.lib
+
+import java.security.MessageDigest
+import java.util.*
+
+class OfferWallHashKeyGenerator(
+    private val digest: MessageDigest = MessageDigest.getInstance("SHA-1")
+) {
+
+    private fun toQueryMap(query: String?): SortedMap<String, String> {
+        val paramsMap = sortedMapOf<String, String>()
+        query?.split("&")?.forEach {
+            val keyValue = it.split("=")
+            paramsMap[keyValue[0]] = keyValue[1]
+        }
+        return paramsMap
+    }
+
+    fun compute(query: String?): String? = with(toQueryMap(query)) {
+        remove("hashkey")
+        val token = remove("token") ?: return null
+
+        val joinedParams = map { "${it.key}=${it.value}" }.joinToString(separator = "&") { it }
+        return "$joinedParams&$token".hash(digest)
+    }
+
+}
+
+fun String.hash(digest: MessageDigest = MessageDigest.getInstance("SHA-1")): String {
+    val result = digest.digest(toByteArray(Charsets.UTF_8))
+    return result.joinToString(separator = "") { String.format("%02X", it) }.lowercase(Locale.ROOT)
+}
