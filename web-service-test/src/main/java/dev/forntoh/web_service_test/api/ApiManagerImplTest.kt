a687ed0d4d0ab4c72e4fe6b2677d5998ed8131f1
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-16 14:30:00 +0100

    re-arranged test module

--- web-service-test/src/main/java/dev/forntoh/web_service_test/api/ApiManagerImplTest.kt
+++
@@ -1,8 +0,0 @@
-package dev.forntoh.web_service_test.api
-
-import dev.forntoh.web_service.api.ApiManager
-import dev.forntoh.web_service.api.MainApiService
-
-class ApiManagerImplTest(
-    override val mainApi: MainApiService
-) : ApiManager
