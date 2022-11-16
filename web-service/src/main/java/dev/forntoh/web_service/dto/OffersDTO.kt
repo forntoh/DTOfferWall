27af3c56f8ed4ba3457a4835e1bb6de1ddeb35cc
forntoh <thomasforntoh@gmail.com>
2022-11-16 11:20:54 +0100

    set default params for OffersDTO

--- web-service/src/main/java/dev/forntoh/web_service/dto/OffersDTO.kt
+++ web-service/src/main/java/dev/forntoh/web_service/dto/OffersDTO.kt
@@ -8,6 +8,6 @@ data class OffersDTO(
     val message: String,
     val count: Int,
     val pages: Int,
-    var offers: List<Offer>,
-    val information: AppInfo,
+    var offers: List<Offer> = emptyList(),
+    val information: AppInfo? = null,
 )
