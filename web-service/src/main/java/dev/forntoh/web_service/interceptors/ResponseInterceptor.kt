b5c55ab4498afb4af81e8aa5ba1bf84b04213f8a
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-16 22:40:08 +0100

    fixed ResponseInterceptor always checking signature on response error

--- web-service/src/main/java/dev/forntoh/web_service/interceptors/ResponseInterceptor.kt
+++ web-service/src/main/java/dev/forntoh/web_service/interceptors/ResponseInterceptor.kt
@@ -1,6 +1,8 @@
 package dev.forntoh.web_service.interceptors
 
+import com.google.gson.Gson
 import dev.forntoh.common.lib.OfferWallHashKeyUtility
+import dev.forntoh.web_service.dto.OffersDTO
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
@@ -15,16 +17,25 @@ class ResponseInterceptor @Inject constructor(
 
         if (response.isSuccessful) {
             val responseString = response.peekBody(Long.MAX_VALUE).string()
-            val signature = response.header("X-Sponsorpay-Response-Signature")
 
-            val isValid = offerWallHashKeyUtility.validate(responseString, signature)
+            val offersDTO = Gson().fromJson(responseString, OffersDTO::class.java)
 
-            if (!isValid)
-                return response
-                    .newBuilder()
-                    .message("WARNING: Response has been altered")
-                    .code(401)
-                    .build()
+            // Only check signature if response is successful
+            if (!offersDTO.code.startsWith("ERROR")) {
+
+                val signature = response.header("X-Sponsorpay-Response-Signature")
+
+                println(offersDTO)
+
+                val isValid = offerWallHashKeyUtility.validate(responseString, signature)
+
+                if (!isValid)
+                    return response
+                        .newBuilder()
+                        .message("WARNING: Response has been altered")
+                        .code(401)
+                        .build()
+            }
         }
         return response
     }
