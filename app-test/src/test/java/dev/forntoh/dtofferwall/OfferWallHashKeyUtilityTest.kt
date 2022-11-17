cd9323f7afd5e7e170f35f3f62a467adc719c551
forntoh <thomasforntoh@gmail.com>
2022-11-17 10:54:17 +0100

    used offerWallHashKeyUtility in OfferWallHashKeyUtilityTest

--- app-test/src/test/java/dev/forntoh/dtofferwall/OfferWallHashKeyUtilityTest.kt
+++ app-test/src/test/java/dev/forntoh/dtofferwall/OfferWallHashKeyUtilityTest.kt
@@ -1,20 +1,20 @@
 package dev.forntoh.dtofferwall
 
-import dev.forntoh.common.lib.OfferWallHashKeyUtility
+import dev.forntoh.dtofferwall.util.offerWallHashKeyUtility
 import org.junit.Test
 import kotlin.test.assertEquals
 
 internal class OfferWallHashKeyUtilityTest {
 
     @Test
-    fun `hash generation is okay`() = with(OfferWallHashKeyUtility()) {
+    fun `hash generation is okay`() = with(offerWallHashKeyUtility) {
         val expectedHash = "e1f1f7c5d56243584942022bc8bdb58c06297bb8" // hash of p1=1&p2=2&1234 see DT Docs
         val actualHash = compute("token=1234&p2=2&p1=1")
         assertEquals(expectedHash, actualHash)
     }
 
     @Test
-    fun `signature is verified correctly`() = with(OfferWallHashKeyUtility()) {
+    fun `signature is verified correctly`() = with(offerWallHashKeyUtility) {
         compute("token=1234&p1=1&p2=2") // Token will be set to 1234
 
         val response = "RESPONSE"
