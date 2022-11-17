bdcbfd7c0e0fc79aaf5336c6042f888e1cf4a822
forntoh <thomasforntoh@gmail.com>
2022-11-17 10:53:51 +0100

    used OfferWallDataSourceProvider in OfferWallNetworkDataSourceTest

--- app-test/src/test/java/dev/forntoh/dtofferwall/OfferWallNetworkDataSourceTest.kt
+++ app-test/src/test/java/dev/forntoh/dtofferwall/OfferWallNetworkDataSourceTest.kt
@@ -1,80 +1,31 @@
 package dev.forntoh.dtofferwall
 
-import com.google.gson.Gson
-import dev.forntoh.common.entities.Offer
 import dev.forntoh.common.entities.OfferFilter
-import dev.forntoh.common.lib.OfferWallHashKeyUtility
 import dev.forntoh.common.lib.hash
-import dev.forntoh.web_service.api.MainApiService
+import dev.forntoh.dtofferwall.util.*
 import dev.forntoh.web_service.datasources.OfferWallNetworkDataSource
-import dev.forntoh.web_service.dto.OffersDTO
-import dev.forntoh.web_service.interceptors.RequestInterceptor
-import dev.forntoh.web_service.interceptors.ResponseInterceptor
 import kotlinx.coroutines.runBlocking
 import mockwebserver3.MockResponse
 import mockwebserver3.MockWebServer
-import okhttp3.OkHttpClient
 import org.junit.After
+import org.junit.Before
 import org.junit.Test
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 class OfferWallNetworkDataSourceTest {
 
-    private val gson = Gson()
+    private lateinit var offerWallDataSourceProvider: OfferWallDataSourceProvider
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var owNetworkDataSource: OfferWallNetworkDataSource
 
-    private val mockWebServer = MockWebServer()
-
-    private val offerWallHashKeyUtility = OfferWallHashKeyUtility()
-
-    private val client = OkHttpClient.Builder()
-        .connectTimeout(1, TimeUnit.SECONDS)
-        .readTimeout(1, TimeUnit.SECONDS)
-        .writeTimeout(1, TimeUnit.SECONDS)
-        .addInterceptor(RequestInterceptor(offerWallHashKeyUtility))
-        .addInterceptor(ResponseInterceptor(offerWallHashKeyUtility))
-        .build()
-
-    private val api = Retrofit.Builder()
-        .baseUrl(mockWebServer.url("/"))
-        .client(client)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(MainApiService::class.java)
-
-    private val owNetworkDataSource = OfferWallNetworkDataSource(ApiManagerImplTest(api))
-
-    private val testOfferFilter = OfferFilter(
-        appId = "1234",
-        userId = "testUser",
-        token = "8a85q89",
-    )
-
-    private val testOffersDTO = OffersDTO(
-        code = "OK",
-        message = "Test Message",
-        count = 1,
-        pages = 10,
-        offers = listOf(
-            Offer(
-                link = "test-ink",
-                offerId = 10,
-                payout = 100,
-                requiredActions = "test-requiredActions",
-                teaser = "test-teaser",
-                title = "test-title",
-                offerTypes = emptyList(),
-                timeToPayout = Offer.TimeToPayout(1800, "30 Minutes"),
-                thumbnail = Offer.Thumbnail("test-hires", "test-low-res")
-            )
-        ),
-    )
-
-    private val signature = "${gson.toJson(testOffersDTO)}${testOfferFilter.token}".hash()
+    @Before
+    fun setUp() {
+        offerWallDataSourceProvider = OfferWallDataSourceProvider()
+        mockWebServer = offerWallDataSourceProvider.mockWebServer
+        owNetworkDataSource = offerWallDataSourceProvider.owNetworkDataSource
+    }
 
     @After
     fun tearDown() {
